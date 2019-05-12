dc85d13fae30d7591185cd188d03be3f6664b06d
kor1k <koortttt@gmail.com>
2019-05-12 22:43:20 +0300

    ....

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,141 @@
 $(document).ready(function ($) {
-    // var disableSlick = {
-    //     arrows: false,
-    //     dots: false,
-    //     responsive: [{
-    //         breakpoint: 99999,
-    //         settings: "unslick"
-    //     }]
-    // };
-    $('.section-star-slider, .section-oil-slider, .section-stocks-slider, .section-exchange_green-slider, .section-exchange_violet-slider').slick({
-        // infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        // destroy: slick,
-        // slickPlay: true,
-        dotsClass: "custom-dots",
-        autoplay: true,
-        autoplaySpeed: 1500,
-        responsive: [{
-            breakpoint: 481,
-            settings: {
-                slidesToShow: 1,
-                arrows: true,
-                dots: true,
-                slidesToScroll: 1
-            }
-        },
-            {
-                breakpoint: 320,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                    dots: false,
-                    slidesToScroll: 1
-                }
-            }]
+    $(function () {
+        'use strict';
+
+        var $swipeTabsContainer = $('.swipe-tabs'),
+            $swipeTabs = $('.swipe-tab'),
+            $swipeTabsContentContainer = $('.swipe-tabs-container'),
+            currentIndex = 0,
+            activeTabClassName = 'active-tab';
+
+        $swipeTabsContainer.on('init', function (event, slick) {
+            $swipeTabsContentContainer.removeClass('invisible');
+            $swipeTabsContainer.removeClass('invisible');
+
+            currentIndex = slick.getCurrent();
+            $swipeTabs.removeClass(activeTabClassName);
+            $('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+        });
+
+        $swipeTabsContainer.slick({
+            //slidesToShow: 3.25,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            infinite: false,
+            swipeToSlide: true,
+            touchThreshold: 10
+        });
+
+        $swipeTabsContentContainer.slick({
+            asNavFor: $swipeTabsContainer,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            infinite: false,
+            swipeToSlide: true,
+            draggable: false,
+            touchThreshold: 10
+        });
+
+
+        $swipeTabs.on('click', function (event) {
+            // gets index of clicked tab
+            currentIndex = $(this).data('slick-index');
+            $swipeTabs.removeClass(activeTabClassName);
+            $('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+            $swipeTabsContainer.slick('slickGoTo', currentIndex);
+            $swipeTabsContentContainer.slick('slickGoTo', currentIndex);
+        });
+
+        //initializes slick navigation tabs swipe handler
+        $swipeTabsContentContainer.on('swipe', function (event, slick, direction) {
+            currentIndex = $(this).slick('slickCurrentSlide');
+            $swipeTabs.removeClass(activeTabClassName);
+            $('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+        });
     });
+
+// var disableSlick = {
+//     arrows: false,
+//     dots: false,
+//     responsive: [{
+//         breakpoint: 99999,
+//         settings: "unslick"
+//     }]
+// };
+//     $('.section-star-slider, .section-oil-slider, .section-stocks-slider, .section-exchange_green-slider, .section-exchange_violet-slider').slick({
+//     // infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     dots: true,
+//     // destroy: slick,
+//     // slickPlay: true,
+//     dotsClass: "custom-dots",
+//     autoplay: true,
+//     autoplaySpeed: 1500,
+//     responsive: [{
+//         breakpoint: 481,
+//         settings: {
+//             slidesToShow: 1,
+//             arrows: true,
+//             dots: true,
+//             slidesToScroll: 1
+//         }
+//     },
+//         {
+//             breakpoint: 320,
+//             settings: {
+//                 slidesToShow: 1,
+//                 arrows: false,
+//                 dots: false,
+//                 slidesToScroll: 1
+//             }
+//         }]
+// });
+
+
+    $(function () {
+        $('#myTab a:last').tab('show')
+    });
+    $('.slider').slick();
+
 
     // active circles (categories)
-    $('.bar-item').click(function () {
-        $('.bar-item').removeClass('active');
-        $(this).toggleClass('active');
-    });
+    // $('.bar-item').click(function () {
+    //     $('.bar-item').removeClass('active');
+    //     $(this).toggleClass('active');
+    // });
 
-    $(document).ready(function () {
-        $('ul.tabs li').click(function () {
-            var tab_id = $(this).attr('data-tab');
-            $('ul.tabs li').removeClass('current');
-            $('.tab-content').removeClass('current');
-            $(this).addClass('current');
-            $("#" + tab_id).addClass('current');
-        })
-    });
+    // $('ul.tabs li').click(function () {
+    //     var tab_id = $(this).attr('data-tab');
+    //     $('ul.tabs li').removeClass('current');
+    //     $('.tab-content').removeClass('current');
+    //     $(this).addClass('current');
+    //     $("#" + tab_id).addClass('current');
+
+    // if (tab_id === 'tab-1') {
+    //     console.log('tab-1')
+    // } else {
+    //     // alert('not ok')
+    //     $('.section-oil-slider').slick('unslick');
+    //     $('.section-stocks-slider').slick('unslick');
+    //     $('.section-exchange_green-slider').slick('unslick');
+    //     $('.section-exchange_violet-slider').slick('unslick');
+    // }
+    // // console.log(tab_id);
+    //
+    // if (tab_id === 'tab-2') {
+    //     console.log('tab-2')
+    // } else {
+    //     $('.section-star-slider').slick('unslick');
+    //     // $('.section-oil-slider').slick('unslick');
+    //     $('.section-stocks-slider').slick('unslick');
+    //     $('.section-exchange_green-slider').slick('unslick');
+    //     $('.section-exchange_violet-slider').slick('unslick');
+    // }
+
+
     //TODO: MAKE IT LIKE TABS. IN TAB SHOULD BE SLICKS WITH RIGHT CONTENT FOR EACH CIRCLE!!!!!!!!!!!!
     //TODO: MAKE IT LIKE TABS. IN TAB SHOULD BE SLICKS WITH RIGHT CONTENT FOR EACH CIRCLE!!!!!!!!!!!!
     //TODO: MAKE IT LIKE TABS. IN TAB SHOULD BE SLICKS WITH RIGHT CONTENT FOR EACH CIRCLE!!!!!!!!!!!!
@@ -84,5 +168,6 @@ $(document).ready(function ($) {
             $('.temp_min-show').html(temp_min);
             $('.temp_max-show').html(temp_max);
         }
-    )
+    );
+
 });
